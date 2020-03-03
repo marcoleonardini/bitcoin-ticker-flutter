737b8b692b7e09da4b2231f4c9239b571320248b
Marco Leonardini <marco_leonardini@hotmail.com>
2020-03-03 16:58:30 -0400

    end challenge App

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import 'coin_data.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +13,30 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  double value = 0;
+
+  List<DropdownMenuItem> getDropdownItems() {
+    List<DropdownMenuItem> items = [];
+    for (String currency in currenciesList) {
+      var item = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      items.add(item);
+    }
+    return items;
+  }
+
+  List<Text> getPickerItems() {
+    List<Text> items = [];
+    for (String currency in currenciesList) {
+      var item = Text(currency);
+      items.add(item);
+    }
+    return items;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +58,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $value $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -38,14 +69,43 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: CupertinoPicker(
+                itemExtent: 32,
+                onSelectedItemChanged: (value) {
+                  getConversion(currenciesList[value]);
+                },
+                children: getPickerItems(),
+              )),
         ],
       ),
     );
   }
+
+  void getConversion(String currency) async {
+    Response r = await get(
+        'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC$currency',
+        headers: {'x-ba-key': 'NzY3ODI2ZGZlMjhlNDRiYThmMzNmZTViM2RmMGE5MzI'});
+    var decoded = jsonDecode(r.body);
+    setState(() {
+      value = decoded['last'];
+      selectedCurrency = currency;
+    });
+    print(value);
+    //
+  }
 }
+
+// DropdownButton(
+//               value: selectedCurrency,
+//               onChanged: (value) {
+//                 print(value);
+//                 setState(() {
+//                   selectedCurrency = value;
+//                 });
+//               },
+//               items: getDropdownItems(),
+//             ),
